a34dfcab169ea88ff428bec7ad41b50b7812f350
artemii <2023.redchuk.artem@ktbp.net.ua>
2025-08-27 12:19:59 +0300

    vlad padla

--- src/app/form/custom.valid.ts
+++ src/app/form/custom.valid.ts
@@ -3,8 +3,23 @@ import {AbstractControl, ValidationErrors } from '@angular/forms';
 export function password_valid(control: AbstractControl): ValidationErrors | null {
   const password = control.get('Password');
   const passwordConfirm = control.get('PasswordConfirm');
-  if (password && passwordConfirm && password.value !== passwordConfirm.value) {
-    return { 'password_valid': false };
+  if (password && passwordConfirm && password.value !== passwordConfirm.value && passwordConfirm.value !== '') {
+    return { 'password_valid': true };
+  }
+  return null;
+}
+
+export function born_valid(control: AbstractControl): ValidationErrors | null {
+  const today = new Date();
+  const age = new Date(control.value);
+  const age_year = today.getFullYear() - age.getFullYear();
+  const age_month = today.getMonth() - age.getMonth();
+  const age_day = today.getDate() - age.getDate();
+  if (age_month < 0 || (age_month === 0 && age_day < 0)) {
+    age_year - 1;
+  }
+  if (age_year < 14) {
+    return { 'born_valid': true };
   }
   return null;
 }
--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators, } from '@angular/forms';
-import {password_valid} from './custom.valid';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {password_valid, born_valid} from './custom.valid';
 
 @Component({
   selector: 'app-form',
@@ -11,16 +11,16 @@ import {password_valid} from './custom.valid';
 })
 
 export class Form {
-  singupForm = new FormGroup({
+  signupForm = new FormGroup({
     Firstname: new FormControl('', {validators: [Validators.required, Validators.minLength(2), Validators.maxLength(50)]}),
     Lastname: new FormControl('', {validators: [Validators.required, Validators.minLength(2), Validators.maxLength(50)]}),
     Email: new FormControl('', {validators: [Validators.required, Validators.email]}),
     Password: new FormControl('', {validators: [Validators.required, Validators.minLength(8), Validators.pattern('(?=.*[0-9])(?=.*[a-zA-Z]).+')]}),
     PasswordConfirm: new FormControl('', {validators: Validators.required}),
-    Born: new FormControl('', Validators.required),
+    Born: new FormControl('', {validators: [Validators.required, born_valid]}),
   }, {validators: password_valid});
   register() {
-    const {Password, PasswordConfirm, ...User} = this.singupForm.value
+    const {Password, PasswordConfirm, ...User} = this.signupForm.value
     console.log(JSON.stringify(User, null, 1));
   }
 }
